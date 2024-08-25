09dd4847eee94a32a75f8d388636efae5664f23c
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-08-25 15:53:07 +0300

    Initial commit

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => { };
 
 addOneContact();
+
+console.log("salam bratcd..");
